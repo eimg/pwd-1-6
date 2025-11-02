ceade3eebbd2ba61190748ea5eb4020a525e37a3
Ei Maung <eimg@fairwayweb.com>
2025-11-02 12:17:21 +0630

    projectu pdate

--- project/tests/module.php
+++ project/tests/module.php
@@ -8,14 +8,14 @@ use Libs\Database\MySQL;
 use Libs\Database\UsersTable;
 use Faker\Factory as Faker;
 
-Auth::check();
-HTTP::redirect();
+// Auth::check();
+// HTTP::redirect();
 
-$db = new MySQL;
-$db->connect();
+// $db = new MySQL;
+// $db->connect();
 
-$table = new UsersTable;
-$table->insert();
+// $table = new UsersTable;
+// $table->insert();
 
-$faker = Faker::create();
-echo $faker->name();
+// $faker = Faker::create();
+// echo $faker->name();
