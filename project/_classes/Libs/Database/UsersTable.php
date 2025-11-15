e82ca20c278811af79d78484476ac599be196f3a
Ei Maung <eimg@fairwayweb.com>
2025-11-15 12:24:01 +0630

    project update

--- project/_classes/Libs/Database/UsersTable.php
+++ project/_classes/Libs/Database/UsersTable.php
@@ -2,10 +2,75 @@
 
 namespace Libs\Database;
 
+use PDOException;
+
 class UsersTable
 {
-    public function insert()
+    private $db;
+
+    public function __construct(MySQL $mysql)
     {
-        echo "UsersTable Insert <br>";
+        $this->db = $mysql->connect();
+    }
+
+    public function all()
+    {
+        $statement = $this->db->query(
+            "SELECT users.*, roles.name AS role
+            FROM users LEFT JOIN roles
+            ON users.role_id = roles.id"
+        );
+
+        return $statement->fetchAll();
+    }
+
+    public function find($email, $password)
+    {
+        try {
+            $statement = $this->db->prepare("SELECT * FROM users WHERE email=:email AND password=:password");
+
+            $statement->execute(['email' => $email, 'password' => $password]);
+
+            return $statement->fetch();
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit();
+        }
+    }
+
+    public function insert($data)
+    {
+        try {
+            $statement = $this->db->prepare(
+                "INSERT INTO users (name, email, phone, password,
+                address, created_at) VALUES (:name, :email, :phone,
+                :password, :address, NOW())"
+            );
+
+            $statement->execute($data);
+
+            return $this->db->lastInsertId();
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit();
+        }
+    }
+
+    public function changePhoto($id, $photo)
+    {
+        $statement = $this->db->prepare("UPDATE users SET photo=:photo WHERE id=:id");
+        $statement->execute(['id' => $id, 'photo' => $photo]);
+
+        return $statement->rowCount();
+    }
+
+    public function delete($id)
+    {
+        $statment = $this->db->prepare("DELETE FROM users WHERE id=:id");
+        $statment->execute(['id' => $id]);
+
+        return $statment->rowCount();
     }
 }
--- project/profile.php
+++ project/profile.php
@@ -1,30 +1,44 @@
 <?php
-    session_start();
-    $login = isset($_SESSION['user']);
+include("vendor/autoload.php");
 
-    if(!$login) {
-        header("location: index.php");
-        exit();
-    }
+use Helpers\Auth;
+
+$user = Auth::check();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container" style="max-width: 800px">
         <h1 class="my-4">Profile</h1>
+
+        <?php if($user->photo): ?>
+            <img src="_actions/photos/<?= $user->photo ?>" width="300" class="img-thumbnail">
+        <?php endif ?>
+
+        <form action="_actions/upload.php" method="post"
+            enctype="multipart/form-data" class="input-group my-3">
+            <input type="file" name="photo" class="form-control">
+            <button class="btn btn-secondary">Upload</button>
+        </form>
+
         <ul class="list-group mb-3">
-            <li class="list-group-item">Name: Alice</li>
-            <li class="list-group-item">Email: alice@gmail.com</li>
-            <li class="list-group-item">Phone: 329842313</li>
-            <li class="list-group-item">Address: Some address</li>
+            <li class="list-group-item">Name: <?= $user->name ?></li>
+            <li class="list-group-item">Email: <?= $user->email ?></li>
+            <li class="list-group-item">Phone: <?= $user->phone ?></li>
+            <li class="list-group-item">Address: <?= $user->address ?></li>
         </ul>
+
+        <a href="admin.php">Admin</a> |
         <a href="_actions/logout.php" class="text-danger">Logout</a>
     </div>
 </body>
+
 </html>
