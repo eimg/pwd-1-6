272c477423faa8bf194dfa992bbd621e40a76b48
Ei Maung <eimg@fairwayweb.com>
2025-11-22 12:17:08 +0630

    laravel blog

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/articles', function() {
+    return 'Articles Route';
+});
+
+Route::get('/articles/detail/{id}', function ($id) {
+    return "Article Detail - $id";
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
