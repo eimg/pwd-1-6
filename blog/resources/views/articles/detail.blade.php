8e88321f7b2513a3fdf863170235deb7ace589c4
Ei Maung <eimg@fairwayweb.com>
2025-11-30 18:00:47 +0630

    hooks/promise

---
+++ blog/resources/views/articles/detail.blade.php
@@ -0,0 +1,31 @@
+@extends("layouts.app")
+
+@section("content")
+    <div class="container" style="max-width: 800px">
+        <div class="card mb-2 border-primary">
+            <div class="card-body">
+                <h3>{{ $article->title }}</h3>
+                <div class="text-muted">
+                    Category: <b>{{ $article->category->name }}</b>,
+                    {{ $article->created_at }}
+                </div>
+                <p>
+                    {{ $article->body }}
+                </p>
+                <a href="{{ url("/articles/delete/$article->id") }}"
+                    class="btn btn-sm btn-outline-danger">Delete</a>
+            </div>
+        </div>
+
+        <ul class="list-group mt-4">
+            <li class="list-group-item active">
+                Comments ({{ count($article->comments) }})
+            </li>
+            @foreach ($article->comments as $comment)
+                <li class="list-group-item">
+                    {{ $comment->content }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endsection
--- blog/resources/views/articles/index.blade.php
+++ blog/resources/views/articles/index.blade.php
@@ -1,17 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Article List</title>
-</head>
-<body>
-    <h1>Article List</h1>
-    <ul>
+@extends("layouts.app")
+
+@section("content")
+    <div class="container" style="max-width: 800px">
+
+        {{ $articles->links() }}
+
+        @if(session("info"))
+            <div class="alert alert-info">
+                {{ session("info") }}
+            </div>
+        @endif
+
         @foreach ($articles as $article)
-            <li>{{ $article['title'] }}</li>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h3>{{ $article->title }}</h3>
+                    <div class="text-muted">
+                        Category: <b>{{ $article->category->name }}</b>,
+                        Comments: <b>{{ count($article->comments) }}</b>,
+                        {{ $article->created_at }}
+                    </div>
+                    <p>
+                        {{ $article->body }}
+                    </p>
+                    <a href="{{ url("/articles/detail/$article->id") }}">
+                        View Detail
+                    </a>
+                </div>
+            </div>
         @endforeach
-    </ul>
-</body>
-</html>
+    </div>
+@endsection
